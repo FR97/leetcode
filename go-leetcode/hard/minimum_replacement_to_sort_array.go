3fd3936fae12cc4586ddbb57d19daf94e6bbe3a5
FR97 <fristic97@gmail.com>
2023-08-30 21:33:13 +0200

    minimum replacement to sort array formatting
    Signed-off-by: FR97 <fristic97@gmail.com>

--- go-leetcode/hard/minimum_replacement_to_sort_array.go
+++ go-leetcode/hard/minimum_replacement_to_sort_array.go
@@ -2,6 +2,7 @@ package hard
 
 func minimumReplacement(nums []int) int64 {
 	result := 0
+
 	for i := len(nums) - 2; i >= 0; i-- {
 		if nums[i] > nums[i+1] {
 			count := (nums[i] + nums[i+1] - 1) / nums[i+1]
