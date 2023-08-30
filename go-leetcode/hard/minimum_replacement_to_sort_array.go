bc4b1ff5ac4eb06e1ee09354d4cced3274afcbd7
FR97 <fristic97@gmail.com>
2023-08-30 21:32:52 +0200

    minimum replacement to sort array problem
    Signed-off-by: FR97 <fristic97@gmail.com>

---
+++ go-leetcode/hard/minimum_replacement_to_sort_array.go
@@ -0,0 +1,14 @@
+package hard
+
+func minimumReplacement(nums []int) int64 {
+	result := 0
+	for i := len(nums) - 2; i >= 0; i-- {
+		if nums[i] > nums[i+1] {
+			count := (nums[i] + nums[i+1] - 1) / nums[i+1]
+			result += count - 1
+			nums[i] = nums[i] / count
+		}
+	}
+
+	return int64(result)
+}
