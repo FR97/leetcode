6896c0467b4b256aac40e8e4834417387af81f6e
FR97 <fristic97@gmail.com>
2023-09-13 21:00:14 +0200

    longest common prefix problem tests
    Signed-off-by: FR97 <fristic97@gmail.com>

--- go-leetcode/medium/longest_common_prefix_test.go
+++ go-leetcode/medium/longest_common_prefix_test.go
@@ -1,1 +1,18 @@
 package medium
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLongestCommonPrefixCase1(t *testing.T) {
+	expected := "fl"
+	actual := longestCommonPrefix([]string{"flower", "flow", "flight"})
+	assert.Equal(t, expected, actual)
+}
+
+func TestLongestCommonPrefixCase2(t *testing.T) {
+	expected := ""
+	actual := longestCommonPrefix([]string{"dog", "racecar", "car"})
+	assert.Equal(t, expected, actual)
+}
