92bb7b36c01c6c39bdec3ddce591ef586736140a
FR97 <fristic97@gmail.com>
2023-09-13 20:58:51 +0200

    longest common prefix problem solution
    Signed-off-by: FR97 <fristic97@gmail.com>

--- go-leetcode/medium/longest_common_prefix.go
+++ go-leetcode/medium/longest_common_prefix.go
@@ -1,5 +1,18 @@
 package medium
 
-func longestCommonPrefix(strs []string) string {
+import "sort"
 
+func longestCommonPrefix(strs []string) string {
+	sort.Strings(strs)
+
+	for i := 0; i < len(strs[0]); i++ {
+		first := strs[0][i]
+		last := strs[len(strs)-1][i]
+		// First and last string would be most different
+		if first != last {
+			return strs[0][:i]
+		}
+	}
+
+	return strs[0]
 }
