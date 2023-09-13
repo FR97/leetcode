9d36ee25db1c6f257d3cf65e305bb3559affb45c
FR97 <fristic97@gmail.com>
2023-09-13 21:02:29 +0200

    fix test import.
    Signed-off-by: FR97 <fristic97@gmail.com>

--- go-leetcode/easy/longest_common_prefix_test.go
+++ go-leetcode/easy/longest_common_prefix_test.go
@@ -1,19 +1,18 @@
 package easy
 
 import (
-	"github.com/FR97/leetcode/go-leetcode/medium"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestLongestCommonPrefixCase1(t *testing.T) {
 	expected := "fl"
-	actual := medium.longestCommonPrefix([]string{"flower", "flow", "flight"})
+	actual := longestCommonPrefix([]string{"flower", "flow", "flight"})
 	assert.Equal(t, expected, actual)
 }
 
 func TestLongestCommonPrefixCase2(t *testing.T) {
 	expected := ""
-	actual := medium.longestCommonPrefix([]string{"dog", "racecar", "car"})
+	actual := longestCommonPrefix([]string{"dog", "racecar", "car"})
 	assert.Equal(t, expected, actual)
 }
